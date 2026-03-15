13cafa74497f8f34ffcff0e92b7197455a3b9a70
ranjan <thisisme.com>
2026-03-15 16:32:48 +0530

    file added

---
+++ demo01.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int arr[5];
+int top=-1;
+
+int main()
+{
+insert(10);
+insert(20);
+show();
+return 0;
+}
+
+void insert(int x)
+{
+if(top==4)
+{
+ cout<<"stack is full";
+}
+else
+{ 
+ top++;
+ arr[top]=x;
+ cout<<"elements inserted"<<endl;
+}
+}
